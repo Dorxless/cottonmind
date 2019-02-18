b77c35c23100ac4ffd243aab610cb7b39054eddb
mohamad92001 <34593460+mohamad92001@users.noreply.github.com>
2019-02-18 15:14:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World2</h1>');
+res.end('<h1>Hello World</h1>');
 });
 server.listen(port,() => {
 console.log("hello")
