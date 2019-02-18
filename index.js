a760a201c9393b50ae5b9625770d76c580c3cb0d
Dorx <36697194+Dorxless@users.noreply.github.com>
2019-02-18 14:42:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(Server running at port +port);
+});
 //Require:
 const api = require('node-telegram-bot-api');
 
