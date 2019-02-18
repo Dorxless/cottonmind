9e61f5e3460afc151ad0f5f8ebc2c08f351907a4
mohamad92001 <34593460+mohamad92001@users.noreply.github.com>
2019-02-18 16:44:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,20 @@
 //Settings:
 
 const settings = {
-    token: 'Put the token from bot father here',
+    token: '742088547:AAH390qBIT10tEhqHt7_VLrU08UO65DSFgY',
     opt: {
         polling: true,
     },
-    myId: 'here put the id you got when you run the bot and send "/getId" ',
-    wellcomMsg: `Here put your Welcome msg that will be sent to 
-the user when send "/start"`
+    myId: '624107725',
+    wellcomMsg: `Welcome !!
+
+Any message you send will be delivered to the bot owner
+.
+.
+Don't worry , your identity won't be known so you can talk freely 
+.
+.
+Follow us on @CottonMinds`
 }
 
 module.exports = settings;
